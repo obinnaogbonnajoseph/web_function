8c69f7abb113d43ed2110ed8d4b09374da34326c
Obinna Ogbonna <obinnaogbonnajoseph@gmail.com>
2022-09-07 20:37:15 +0100

    initial commit

---
+++ src/test/java/com/obinna/web_function/WebFunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obinna.web_function;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
